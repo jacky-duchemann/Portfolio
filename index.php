c264986f4d79d2d2a639d07088cd52d774ada167
jacky-duchemann <jackyduchemann@hotmail.fr>
2024-11-05 12:24:46 +0400

    Barre de compétences et CV

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
     <section class="about-me" >
         <!-- Texte introductif a propos de moi -->
         
-            <div class="about-me__text" >
+            <div class="about-me__text" id="a-propos" >
                 <h2>À propos de moi</h2>
                 <p>Je m'appelle Jacky Duchemann et je suis développeur web spécialisé dans la création de sites WordPress. Passionné par le code depuis plusieurs années, j'aime transformer des idées en sites fonctionnels et intuitifs.</p>
                 <p>Avec plusieurs projets réalisés en PHP, JavaScript et WordPress, je maîtrise les outils modernes du développement web.</p>
@@ -50,6 +50,85 @@
             </div>
         
     </section>
+    
+    <section class="a-propos" >
+    
+    <div class="cv-sections" id="cv">
+        <h2> Mon CV </h2>
+        <div class="cv-section">
+            <h3 class="cv-title" onclick="toggleSection(this)">Formation <span class="icone-toggle">+</span></h3>
+            <div class="cv-content">
+                <p>&diams; <strong>2007/2008 :</strong> Collège Joseph Hubert – Obtention du Brevet des collèges</p>
+                <p>&diams; <strong>2010/2011 :</strong> Lycée Pierre Poivre – Obtention du Baccalauréat 
+                (Scientifique option Science et vie de la terre)</p>
+                <p>&diams;<strong> 2012/2013 :</strong> Lycée Pierre Poivre – Obtention du BTS SIO (Services Informatiques aux 
+                organisations, option solutions d'infrastructure, systèmes et réseaux)<br><br>
+                Cette formation permet d’acquérir des compétences dans trois domaines :<br><br>
+                <strong>Support et mise à disposition de services informatiques :</strong> Garantir la disponibilité des services et répondre aux besoins des utilisateurs, tout en accompagnant la transformation numérique.<br><br>
+                <strong>Cybersécurité :</strong> Protéger les infrastructures réseau et analyser les risques, en sécurisant les données et l'identité numérique de l'entreprise.<br><br>
+                <strong>Administration des systèmes et réseaux :</strong> Concevoir, installer, tester et déployer des solutions d'infrastructure, tout en administrant et supervisant les réseaux.</p>
+                <p>&diams; <strong>2024 - 13 février au 13 novembre :</strong> Formation de Développeur WordPress avec Openclassrooms.<br>
+                Cette formation m'a permis d'apprendre à créer des sites web avec WordPress, en développant mes compétences en front-end et en personnalisant des thèmes et des plugins. 
+                J'ai été formé à des technologies clés telles que WordPress, PHP, HTML, CSS, JavaScript, ainsi qu'à des outils comme Git & GitHub et Elementor.</p>
+            </div>
+        </div>
+    
+        <div class="cv-section">
+            <h3 class="cv-title" onclick="toggleSection(this)">Expérience <span class="icone-toggle">+</span></h3>
+            <div class="cv-content">
+                <p>&diams; <strong>2012 - Mars à Avril :</strong> Stagiaire dans le collége Joseph Suacot<br><br>
+                Stagiaire aux services de l'administrateur réseaux du collége, dans le cadre de mon BTS.<br>
+                Projet de stage : Réinstallation(logiciel et physique), rénovation et remise en service de la salle informatique du collége</p>
+                <p>&diams; <strong>2013 - Avril à Mai :</strong> Stagiaire dans l'association E.I.L.P Informatique<br><br>
+                Stagiaire dans le cadre du BTS dans une assiociation de jeune en difficulté.<br><br>
+                Installation matériel et logiciel d'un parc informatique, sous la supervisionde l'administrateurréseaux de l'association.<br><br>
+                Accompagnement des utilisateurs sur les différents outils informatiques.</p>
+                <p>&diams; <strong>Octobre 2013 à Juillet 2014 :</strong> Accompagnant scolaire à l'AJMD<br><br>
+                Elargir les centres d'intérêt des jeunes par l'introduction à de nouvelles activités culturelles et à la construction de projets collectifs.<br><br>
+                Proposer un accompagnement adapté aux besoins de chaque enfant.<br><br>
+                Aide aux devoirs scolaires pour élève en difficulté d'apprentissage.</p>
+                <p>&diams; <strong>Septembre 2015 à Août 2021 :</strong> Assistant d'éducation TICE employé par le lycée Paul Langevin<br><br>
+                Aide à l'utilisation des nouvelles technologies aux enfants, de Petite Section jusqu'au CM2.<br><br>
+                Aide au travail scolaire<br><br>
+                Prise en charge de groupes d'éléves en difficulté.</p>
+            </div>
+        </div>
+    </div>
+
+    <section class="competences fade__in__section">
+        <h2> Mes compétences </h2>
+
+        <div class="competence">
+            <span class="competence-name">HTML</span>
+            <div class="competence-bar">
+                <div class="competence-level" style="width: 90%;"></div>
+            </div>
+            <p>Structuration efficace de pages web en utilisant des balises sémantiques et les bonnes pratiques.</p>
+        </div>
+        <div class="competence">
+            <span class="competence-name">SCSS  &rarr; CSS</span>
+            <div class="competence-bar">
+                <div class="competence-level" style="width: 80%;"></div>
+            </div>
+            <p>Création de mises en page modernes et responsives avec Flexbox et Grid. Animation CSS pour enrichir l'expérience utilisateur, en plus d'une bonne maîtrise de SCSS pour une gestion avancée du style.</p>
+        </div>
+        <div class="competence">
+            <span class="competence-name">PHP</span>
+            <div class="competence-bar">
+                <div class="competence-level" style="width: 50%;"></div>
+            </div>
+            <p>Conception de fonctionnalités dynamiques pour WordPress, gestion des templates et custom post types. Code maintenable et respect des bonnes pratiques. </p>
+        </div>
+        <div class="competence">
+            <span class="competence-name">Javascript</span>
+            <div class="competence-bar">
+                <div class="competence-level" style="width: 65%;"></div>
+            </div>
+            <p>Manipulation dynamique du DOM et création d'interactions riches.</p>
+        </div>
+    </section>
+</section>
+
     <section class="liste-projet" id="projets">
         <!-- Titre = Mes projets -->
         <!-- Loop des projets -> 2 colonnes -->
--- js/script.js
+++ js/script.js
@@ -71,8 +71,54 @@ document.addEventListener("DOMContentLoaded", function() {
   })
 });
 
-/* ouverture modale au clic sur les CTA */
-document.addEventListener("DOMContentLoaded", function() {
-  const btnContact = document.querySelectorAll('.btn-contact');
+
+/* animation des barres de compétences */
+
+document.addEventListener("DOMContentLoaded", () => {
+  const SectionCompetence = document.querySelector(".competences");
+  const NiveauCompetence = document.querySelectorAll(".competence-level");
+
+  // Fonction qui remplit les barres de compétences
+  const fillSkillBars = () => {
+    NiveauCompetence.forEach((competence) => {
+          const level = competence.getAttribute("style");
+          competence.style.width = "0"; // Initialise la largeur à 0
+          setTimeout(() => {
+              competence.setAttribute("style", level); // Remplit la barre
+          }, 1300);
+      });
+  };
+  // Fonction qui réinitialise les barres de compétences
+  /*const resetSkillBars = () => {
+    NiveauCompetence.forEach((competence) => {
+        competence.style.width = "0"; // Réinitialise la largeur à 0
+    });
+  };*/
   
-})
+
+  // Création de l'observateur pour détecter l'entrée dans le champ de vision
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+          if (entry.isIntersecting) { // Si la section est visible
+            fillSkillBars();
+          }
+      });
+  });
+
+  observer.observe(SectionCompetence); // Démarre l'observation de la section compétences
+});
+
+
+/* affichage CV */
+function toggleSection(element) {
+  const content = element.nextElementSibling; // Sélectionne le contenu suivant l'élément cliqué
+  const Icon = element.querySelector('.icone-toggle');
+  if (content.style.display === "block") {
+      content.style.display = "none"; // Masque le contenu si visible
+      Icon.textContent = "+";
+  } else {
+      content.style.display = "block"; // Affiche le contenu
+      Icon.textContent = "-";
+  }
+}
+
